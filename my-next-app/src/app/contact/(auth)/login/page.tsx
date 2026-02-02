00a05f659c0c437db5ecb636c0327560bc0909b1
Pratik Pahari <90968509+Pratikrockz@users.noreply.github.com>
2026-02-02 22:47:55 +0530

    dynamic file,group file,routes

---
+++ my-next-app/src/app/contact/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './login.module.css'
+
+export default function loginPgae() {
+  return (
+    <div className={styles.login}>loginPgae</div>
+  )
+}
---
+++ my-next-app/src/app/contact/new/page.tsx
@@ -0,0 +1,9 @@
+
+
+import React from 'react'
+
+export default function newPage() {
+  return (
+    <div>newPage</div>
+  )
+}
